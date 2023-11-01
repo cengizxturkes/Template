8af9c1890fb2b8b1e7daa7c3d5ece4d5a5aaf0e9
Cengiz Türkeş <103938055+cengizxturkes@users.noreply.github.com>
2023-11-01 20:23:09 +0300

    Initial commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,14 @@
+class Constants {
+  static const baseUrl = 'https://jsonplaceholder.typicode.com';
+  static const todosApiUrl = baseUrl + '/todos';
+}
+
+
+/**
+ * 
+ * Hub 
+ *  listen user
+ *      send background change
+ *  
+ * 
+ */
